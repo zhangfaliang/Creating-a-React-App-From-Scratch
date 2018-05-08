44922b761e8d7c65ec8302d87277d3734d7f65b0
张发亮 <fl.zhang@8win.com>
2018-05-08 09:47:37 +0800

    add  chunk hash

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,12 @@
 const merge = require('webpack-merge');
+const webpack = require('webpack')
+
 const common = require('./webpack.common.js')
 
 module.exports = merge(common,{
+  output:{
+    filename: '[name].[chunkhash].js',
+  },
+
   mode:'production'
 })
